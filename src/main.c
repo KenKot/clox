01fd5835e38ec8d78ef8e36d7597c3e64cc8b7e5
Kenny <kennykotten@gmail.com>
2025-08-04 16:38:32 -0700

    begin VM

--- src/main.c
+++ src/main.c
@@ -3,17 +3,22 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[]) {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
     int constant = addConstant(&chunk, 1.2); // returns the index for assocated value in the constant pool array
-    writeChunk(&chunk, OP_CONSTANT);
-    writeChunk(&chunk, constant);
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
 
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
