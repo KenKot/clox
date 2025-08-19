7d2949b2dd180cb659fc25baf9f7ea72bc5e47ab
Kenny <kennykotten@gmail.com>
2025-08-18 19:48:59 -0700

    begin scanner

--- src/vm.c
+++ src/vm.c
@@ -1,4 +1,5 @@
 #include "chunk.h"
+#include "compiler.h"
 #include "common.h"
 #include "debug.h"
 #include "value.h"
@@ -83,10 +84,9 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code; // ip = instruction pointer
-    return run();
+InterpretResult interpret(const char* source) {
+    compile(source);
+    return INTERPRET_OK;
 }
 
 
