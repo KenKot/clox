44bdfe2a037df85f34dbcaa0772daf698c4982d9
Kenny <kennykotten@gmail.com>
2025-09-01 12:56:24 -0700

    add variable declarations

--- src/compiler.c
+++ src/compiler.c
@@ -139,6 +139,19 @@ static void endCompiler() {
 
 static void expression();
 
+static void varDeclaration() {
+    uint8_t global = parseVariable("Expect variable name.");
+
+    if (match(TOKEN_EQUAL)) {
+        expression();
+    } else {
+        emitByte(OP_NIL);
+    }
+    consume(TOKEN_SEMICOLON, "Expect ';' after variable declaration.");
+
+    defineVariable(global);
+}
+
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after value.");
@@ -146,31 +159,34 @@ static void printStatement() {
 }
 
 static void synchronize() {
-  parser.panicMode = false;
+    parser.panicMode = false;
 
-  while (parser.current.type != TOKEN_EOF) {
-    if (parser.previous.type == TOKEN_SEMICOLON) return;
-    switch (parser.current.type) {
-      case TOKEN_CLASS:
-      case TOKEN_FUN:
-      case TOKEN_VAR:
-      case TOKEN_FOR:
-      case TOKEN_IF:
-      case TOKEN_WHILE:
-      case TOKEN_PRINT:
-      case TOKEN_RETURN:
-        return;
+    while (parser.current.type != TOKEN_EOF) {
+        if (parser.previous.type == TOKEN_SEMICOLON) return;
+        switch (parser.current.type) {
+            case TOKEN_CLASS:
+            case TOKEN_FUN:
+            case TOKEN_VAR:
+            case TOKEN_FOR:
+            case TOKEN_IF:
+            case TOKEN_WHILE:
+            case TOKEN_PRINT:
+            case TOKEN_RETURN:
+                return;
 
-      default:
-        ; // Do nothing.
+            default:;  // Do nothing.
+        }
+
+        advance();
     }
-
-    advance();
-  }
 }
 
 static ParseRule *getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
+
+static uint8_t identifierConstant(Token *name) {
+    return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
 
 static void binary() {
     TokenType operatorType = parser.previous.type;
@@ -324,6 +340,15 @@ static void parsePrecedence(Precedence precedence) {
     }
 }
 
+static uint8_t parseVariable(const char *errorMessage) {
+    consume(TOKEN_IDENTIFIER, errorMessage);
+    return identifierConstant(&parser.previous);
+}
+
+static void defineVariable(uint8_t global) {
+  emitBytes(OP_DEFINE_GLOBAL, global);
+}
+
 static ParseRule *getRule(TokenType type) { return &rules[type]; }
 static void statement();
 static void declaration();
@@ -336,6 +361,11 @@ static void expressionStatement() {
 }
 
 static void declaration() {
+    if (match(TOKEN_VAR)) {
+        varDeclaration();
+    } else {
+        statement();
+    }
     statement();
     if (parser.panicMode) synchronize();
 }
--- src/debug.c
+++ src/debug.c
@@ -45,6 +45,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_TRUE", offset);
         case OP_FALSE:
             return simpleInstruction("OP_FALSE", offset);
+        case OP_DEFINE_GLOBAL:
+            return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
         case OP_EQUAL:
             return simpleInstruction("OP_EQUAL", offset);
         case OP_GREATER:
@@ -55,6 +57,9 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_ADD", offset);
         case OP_SUBTRACT:
             return simpleInstruction("OP_SUBTRACT", offset);
+        case OP_POP:
+            return simpleInstruction("OP_POP", offset);
+
         case OP_MULTIPLY:
             return simpleInstruction("OP_MULTIPLY", offset);
         case OP_DIVIDE:
