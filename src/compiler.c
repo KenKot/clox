b5f49faacce6032ffc458d51e14b7919a3378022
Kenny <kennykotten@gmail.com>
2025-09-02 13:29:29 -0700

    block statements

--- src/compiler.c
+++ src/compiler.c
@@ -154,6 +154,14 @@ static void endCompiler() {
 #endif
 }
 
+static void beginScope() {
+  current->scopeDepth++;
+}
+
+static void endScope() {
+  current->scopeDepth--;
+}
+
 static void expression();
 
 static void varDeclaration() {
@@ -389,6 +397,14 @@ static void statement();
 static void declaration();
 static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void expressionStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expect ';' after expression.");
@@ -408,6 +424,10 @@ static void declaration() {
 static void statement() {
     if (match(TOKEN_PRINT)) {
         printStatement();
+    } else if (match(TOKEN_LEFT_BRACE)) {
+        beginScope();
+        block();
+        endScope();
     } else {
         expressionStatement();
     }
